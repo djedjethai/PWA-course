b42ca3e8800bf7f516f58981291e9851320a3c77
djedjethai <djedje.thai.ok@gmail.com>
2020-06-26 20:48:32 +0700

    signup almost done

--- public/auth/jsA/signup.js
+++ public/auth/jsA/signup.js
@@ -1,13 +1,10 @@
-var form = document.querySelector('form');
-
-
 class VerifForm {
 
 	construct(){
 		this.formCompleted = false;  
 	}
 
-	surligne(champ, erreur)
+	static surligne(champ, erreur)
 	{
    		if(erreur)
       			champ.style.backgroundColor = "#fba";
@@ -15,7 +12,7 @@ class VerifForm {
       			champ.style.backgroundColor = "";
 	} 
 
-	verifEmail(champ)
+	static verifEmail(champ)
 	{
    		var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
    		if(!regex.test(champ.value))
@@ -30,7 +27,7 @@ class VerifForm {
    		}
 	}
 
-	verifPassword(champ)
+	static verifPassword(champ)
 	{
    		if(champ.value.length < 2 || champ.value.length > 25)
    		{
@@ -44,7 +41,7 @@ class VerifForm {
    		}
 	}
 
-	verifConfirmPassword(champ)
+	static verifConfirmPassword(champ)
 	{
    		var password = document.querySelector('#password');
    		if(champ.value !== password.value)
@@ -59,18 +56,16 @@ class VerifForm {
    		}
 	}
 
-	verifFinal(f)
+	static verifFinal(f)
 	{
    		var emailOk = this.verifEmail(f.email);
    		var passwordOk = this.verifPassword(f.password);
    		var confirmPasswordOk = this.verifConfirmPassword(f.confirmPassword);
 
-		console.log(this.formCompleted);
-
    		if(emailOk && passwordOk && confirmPasswordOk)
    		{
-      			this.formCompleted = true;	
-      			return true;
+			this.formCompleted = true;
+			return true;
    		} 
    		else
    		{
@@ -79,14 +74,16 @@ class VerifForm {
    		}
 	}
 
-	eventListenerHandler()
+	static eventListenerHandler()
 	{
 		var word = document.querySelector('#password');
 		var email = document.querySelector('#email');
 		var confirmPassword = document.querySelector('#confirmPassword');
+			
 		if (this.formCompleted) {
+		    console.log('envoi form');
 		    this.formCompleted = false;
-			console.log('envoi form');/*
+						
 		    const formData = {
 				email: email.value,
 				password: password.value,
@@ -96,172 +93,22 @@ class VerifForm {
 			    method:'POST',
 			    body:JSON.stringify(formData),
 			    headers: {'Content-type':'application/json'}
-		    })
-				.then(res => {
-					// save webtoken in the browser
-					// and make sur that is not lose if user reload page
-				})
-				.catch(e => console.log(e))*/
-		}
-		else {
-			console.log('fom incomplet pas d\'envoi');
+		    	})
+			.then(res => {
+				// save webtoken in the browser
+				// and make sur that is not lose if user reload page
+			})
+			.catch(e => console.log(e));
+		} else {
+			console.log('pas d envoi');
 		}
 	}
 }
 
-var verifForm = new VerifForm;
-
-verifForm.formCompleted = true; // pb here as the property est modifiable...
-
-console.log(verifForm.formCompleted);
-
+var form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
-	verifForm.eventListenerHandler();
-})
+	VerifForm.eventListenerHandler();
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var formCompleted = false;
-
-function surligne(champ, erreur)
-{
-   if(erreur)
-      champ.style.backgroundColor = "#fba";
-   else
-      champ.style.backgroundColor = "";
-}
-
-function verifEmail(champ)
-{
-   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
-   if(!regex.test(champ.value))
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }
-}
-
-function verifPassword(champ)
-{
-   if(champ.value.length < 2 || champ.value.length > 25)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }
-}
-
-function verifConfirmPassword(champ)
-{
-   var password = document.querySelector('#password');
-   if(champ.value !== password.value)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }
-}
-
-function verifForm(f)
-{
-   var emailOk = verifEmail(f.email);
-   var passwordOk = verifPassword(f.password);
-   var confirmPasswordOk = verifConfirmPassword(f.confirmPassword);
-   
-   if(emailOk && passwordOk && confirmPasswordOk)
-   {
-      formCompleted = true;	
-      return true;
-   } 
-   else
-   {
-      alert("Veuillez remplir correctement tous les champs");
-      return false;
-   }
-}
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    var password = document.querySelector('#password');
-    var email = document.querySelector('#email');
-    var confirmPassword = document.querySelector('#confirmPassword');
-    if (formCompleted) {
-	formCompleted = false;
-	    console.log('envoi form');
-    	const formData = {
-		    email: email.value,
-		    password: password.value,
-		    confirmPassword: confirmPassword.value
-	}; 
-	fetch('http://localhost:3000/auth/authSignup',{
-		method:'POST',
-		body:JSON.stringify(formData),
-		headers: {'Content-type':'application/json'}
-	})
-		    .then(res => {
-			    // save webtoken in the browser
-			    // and make sur that is not lose if user reload page
-		    })
-		    .catch(e => console.log(e))
-    }
-    else {
-	    console.log('fom incomplet pas d\'envoi');
-    }
-    })*/
--- server/controllers/authCt.js
+++ server/controllers/authCt.js
@@ -1,35 +1,59 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 const Auth = require('../models/authMd');
 
 exports.postSignup = (req, res, next) => {
-//	console.log('welcome in controller');
-//	console.log(req.body);
+	
+	// const errors = validationResult(req);
+	// if (!errors.isEmpty) {
+	//	const error = new Error('credential validation error');
+	//	error.statusCode = 422;
+	//	error.data = errors.array();
+	//	throw error;
+	// } 
 
-	const signUp = new Auth({
-		email: req.body.email,
-		password: req.body.password,
-		confirmPassword: req.body.confirmPassword
-	});
 
-	console.log(signUp);
+	var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   	if (regex.test(req.body.email)
+	&& req.body.password.length > 2 && req.body.password.length < 25
+	&& req.body.password === req.body.confirmPassword){
 
-	signUp.save()
+			console.log(signUp);
+	} else {
+		// bloque registration res.err
+		return;
+	}
+
+		
+
+	// bcrypt pw 
+	bcrypt.hash(req.body.password, 12)
+		.then(hashedPw => {
+			const signUp = new Auth({
+				email: req.body.email,
+				password: hashedPw
+			});
+			return signUp;
+		})
+		.then(userCdl => {
+			return userCdl.save()
+		})
 		.then(response => {
 			console.log('retour de db')
 			console.log(response);
 			/* response
-			  _id: 5ef09da63a29aa1560aff579,
-  			  email: 'jerome@jerome.com',
-			  password: 'ddd',
-			  confirmPassword: 'ddd',
-			  createdAt: 2020-06-22T12:01:42.385Z,
-			  updatedAt: 2020-06-22T12:01:42.385Z,
-			  __v: 0
+			_id: 5ef09da63a29aa1560aff579,
+  			email: 'jerome@jerome.com'			  
+			password: 'ddd',
+			confirmPassword: 'ddd',
+			createdAt: 2020-06-22T12:01:42.385Z,
+			 updatedAt: 2020-06-22T12:01:42.385Z,
+			 __v: 0
 			*/
-
-			// integre bcryptejs
-			// on crypte et save en bdd
+		
 
 			// jsonwebtoken
 			// create token and send back to front
@@ -37,3 +61,6 @@ exports.postSignup = (req, res, next) => {
 		})
 		.catch(e => console.log(e));
 }
+
+
+
